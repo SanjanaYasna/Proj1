c17704e1d8643b2419e80a9f869cfefe39c9ac7a
SanjanaYasna <sanyasna517@gmail.com>
2023-09-22 20:37:50 -0400

    no fucking idea

--- Scrape/eBay.py
+++ Scrape/eBay.py
@@ -1,31 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
-query = "women's cleats" #make query serach bar input
-productDictionary = {}
-def find(query):
-    URL = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p4432023.m570.l1313&_nkw=" + query + "&_sacat=0"
-    page = requests.get(URL)
-    productOutput = []
-    soup = BeautifulSoup(page.content, "html.parser")
-    count = 0
-    for item in soup.find_all("div", class_="s-item__info clearfix")[1:]:
-        if count == 8:
-            break
-        productDictionary.update({item.find("span", {'role': 'heading'}).get_text():item.find('span', class_ = "s-item__price").get_text()})
-        #print(item.find('span', class_ = "s-item__price").get_text())
-        count += 1
-#name of item: after _nkw, before & symbol
-#if name has 's, 's -> %27s%20
-find(query)
 
-print(productDictionary)
+from django import forms
+class Find(forms.Form):
+    def getQuery(request):
+        query = request.POST.get("")
+    query = "women's cleats" #make query serach bar input
+    productDictionary = {}
+    def find(query):
+        URL = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p4432023.m570.l1313&_nkw=" + query + "&_sacat=0"
+        page = requests.get(URL)
+        productOutput = []
+        soup = BeautifulSoup(page.content, "html.parser")
+        count = 0
+        for item in soup.find_all("div", class_="s-item__info clearfix")[1:]:
+            if count == 8:
+                break
+            productDictionary.update({item.find("span", {'role': 'heading'}).get_text():item.find('span', class_ = "s-item__price").get_text()})
+            #print(item.find('span', class_ = "s-item__price").get_text())
+            count += 1
+    #name of item: after _nkw, before & symbol
+    #if name has 's, 's -> %27s%20
+    find(query)
 
-import keyword
-#isKeyword function:
-tShirt = "Ralph lauren T-shirt"
-'''for i in productDictionary:
-    if tShirt.iskeyword(productDictionary[0]):
-        print(productDictionary[0])'''
+    '''print(productDictionary)
+
+    import keyword
+    #isKeyword function:
+    tShirt = "Ralph lauren T-shirt"
+    for i in productDictionary:
+        if tShirt.iskeyword(productDictionary[0]):
+            print(productDictionary[0])'''
 
 
 
--- Scrape/keyFoods.py
+++ Scrape/keyFoods.py
@@ -1,32 +1,12 @@
-import requests
-from bs4 import BeautifulSoup
+#from js import document 
 
-URL = "https://www.keyfood.com/store/keyFood/en/search/?text=kittens"
-page = requests.get(URL)
+'''def __init__(self):
+    self.query = Element("key_Foods_Search").element.value'''
+def find(*args, **kwargs):
+    query = Element('key_Foods_Search').element.value
+    query = str(query)
+    Element('test-output').element.innerText = query
+        
+            
 
-#Maybe not add set for other ones, where same names but different prices exist. Here, we only care about brand
-productOutput = set()
-soup = BeautifulSoup(page.content, "html.parser")
-productName = "product__list--name h-reset"
-count = 0
-
-'''
-#smaple code without set count, most liekly delete soon
-resProducts = soup.find_all('h3', class_ = productName, limit = 8)
-for product in resProducts:
-    product = str(product)
-    product = product.replace('<h3 class="product__list--name h-reset">', '')
-    product = product.replace('</h3>', '')
-    productOutput.add(product)'''
-#get set of 8 unique results
-for product in soup.find_all('h3', class_ = productName, limit = 8):
-    if count == 8: 
-        break
-    product = str(product)
-    product = product.replace('<h3 class="product__list--name h-reset">', '')
-    product = product.replace('</h3>', '')
-    productOutput.add(product)
-    count = len(productOutput)
-print(productOutput)
-
-#print(soup)
+    #Element("key_Foods_Search").element.oninput = food_display_handler
