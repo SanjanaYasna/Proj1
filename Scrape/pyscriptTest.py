d014d8a81ee28f415ce3f2a4741bd749f8f6ff07
SanjanaYasna <sanyasna517@gmail.com>
2023-09-26 00:20:39 -0400

    restore

--- Scrape/pyscriptTest.py
+++
@@ -1,8 +0,0 @@
-#from bs4 import BeautifulSoup
-#consider renaming this to original file names (and adjust the file names under previousattempts...)
-import requests
-def find(*args, **kwargs):
-    product= Element('test-input').element.value
-    Element('test-output').element.innerText = product
-    
-
